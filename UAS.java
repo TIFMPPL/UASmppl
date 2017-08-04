cfee2ef65259fbc70bf9798e0792f9a9e56b12ab
mayhendrapandya <mayhendrapandyasahasika@gmail.com>
2017-08-04 10:54:18 +0700

    Update UAS.java

--- UAS.java
+++ UAS.java
@@ -1,13 +1,13 @@
 public class UAS {
     public static void main(String[] args) {
         Student person = new Student();
-        person.setName("Andy");
-	person.setNIM("067001700001");
+        person.setName("Rilo");
+	person.setNIM("065001600013");
         person.setProdi("Sistem Informasi");
         System.out.println(person);
 
 	// Ubah 'Sarah' dengan nama anda sendiri
-        Student mhs = new Student("Sarah");
+        Student mhs = new Student("Rilo");
 	// Tulis statement untuk menset NIM anda sendiri
 	...;
 	// Tulis statement untuk menset program studi anda
